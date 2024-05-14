5481f06d2700691c10047407c39b56d67508232a
Mykola Darnichenko <programmboy@gmail.com>
2024-05-14 16:02:50 +0300

    edit reviews, header, footer

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,17 @@
-// let menuBtn = document.querySelector('.menu-btn');
-// let menu = document.querySelector('.mobile-menu');
-// menuBtn.addEventListener('click', function(){
-// 	menu.classList.toggle('active');
-// })
-
-
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.mobile-menu');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
-  menu.classList.contains('active') ? menu.style.display = 'block' : menu.style.display ='none';
+  
+    if (!menu.classList.contains('active')) {
+    contactTitle.classList.remove('active');
+  }
+  // menu.classList.contains('active') ? menu.style.display = 'block' : menu.style.display ='none';
+})
+
+const contactTitle = document.querySelector('.contact-title');
+
+contactTitle.addEventListener('click', () => {
+  contactTitle.classList.toggle('active');
 })
